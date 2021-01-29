0c848ca3028915e1fb05afbbf2bfda6ea7ca138d
Isadora Brito <isadoradpbrito@gmail.com>
2021-01-29 09:38:12 -0300

    lista02

---
+++ Ex018.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Ex018 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int ianos,imeses,idias,totaldias;
+
+        System.out.print("Digite a sua idade em anos,meses e dias: ");
+        ianos = in.nextInt();
+        imeses = in.nextInt();
+        idias = in.nextInt();
+
+        totaldias = idias + (imeses*30) + (ianos*365);
+        
+        System.out.println("Você viveu "+totaldias+" dias até hoje.");
+    }
+}
