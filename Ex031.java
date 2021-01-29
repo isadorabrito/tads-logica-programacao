ac6a437bff85b499110183120d813c4e011fc651
Isadora Brito <isadoradpbrito@gmail.com>
2021-01-29 19:45:56 -0300

    lista02

---
+++ Ex031.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Ex031 {
+    public static void main(String[] args) {
+
+
+        Scanner in = new Scanner(System.in);    
+        
+        float comprimento, largura,altura;
+        int tcaixas;
+
+        System.out.print("Digite as dimensões da cozinha (comprimento x largura x altura): ");
+        comprimento = in.nextFloat();
+        largura = in.nextFloat();
+        altura = in.nextFloat();
+
+        tcaixas = (int) (((comprimento*altura)*2 + (largura*comprimento)*2)/1.5f);
+        System.out.println("Será necessário "+tcaixas+ " caixas de azulejos.");
+    }
+}
