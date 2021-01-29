87e0944aeb7e1925e35f1ec72a21c9420b372374
Isadora Brito <isadoradpbrito@gmail.com>
2021-01-29 13:43:36 -0300

    lista02

---
+++ Ex020.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Ex020 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int teleitores,vbrancos,vnulos,vvalidos,pvalidos,pbrancos,pnulos;
+        
+        System.out.print("Digite o número de eleitores: ");
+        teleitores = in.nextInt();
+        System.out.print("Digite o número de votos válidos ");
+        vvalidos = in.nextInt();
+        System.out.print("Digite o número de votos em branco: ");
+        vbrancos = in.nextInt();
+        System.out.print("Digite o número de votos nulos: ");
+        vnulos = in.nextInt();
+
+        pvalidos = (vvalidos*100)/teleitores;
+        pbrancos = (vbrancos*100)/teleitores;
+        pnulos = (vnulos*100) /teleitores;
+
+        System.out.printf("O percentual de votos válidos foi %d%%. %nO percentual de votos brancos foi %d%%. %nE o percentual de votos nulos foi %d%%. %n", pvalidos, pbrancos, pnulos);
+    }
+}
