2fd52142d815b513fa3a1619d430a1ac19246af4
Isadora Brito <isadoradpbrito@gmail.com>
2021-01-29 09:10:02 -0300

    lista02

---
+++ Ex016.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Ex016 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int n1, n2;
+
+        System.out.print("Digite dois números inteiros: ");
+        n1 = in.nextInt();
+        n2 = in.nextInt();
+
+        if (n1>n2) {
+            System.out.println("O primeiro valor (" +n1+ ") é o maior.");
+        } else {
+            System.out.println("O segundo valor (" +n2+ ") é o maior.");
+        }
+    }
+}
