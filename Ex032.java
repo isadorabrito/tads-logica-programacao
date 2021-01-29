c7380bb33da41458addda1aa69b555ea65c86ba0
Isadora Brito <isadoradpbrito@gmail.com>
2021-01-29 20:36:37 -0300

    lista02

---
+++ Ex032.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Ex032 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        float iniodometro,fimodometro,litroscomb,tvalor,mediaconsumo,lucro,comb;
+
+        System.out.print("Digite a marcação do odômetro no inicio e fim do dia: ");
+        iniodometro = in.nextFloat();
+        fimodometro = in.nextFloat();
+        System.out.print("Digite o número de litros de combustivel gasto: ");
+        litroscomb = in.nextFloat();
+        System.out.print("Digite o valor total recebido dos passageiros: ");
+        tvalor = in.nextFloat();
+
+        comb = 4.599f;
+        mediaconsumo = (fimodometro - iniodometro)/(litroscomb);
+        lucro = tvalor - (comb*litroscomb);
+
+        System.out.printf("A média de consumo é %.1f Km/L. %n", mediaconsumo);
+        System.out.printf("O lucro é de R$ %.2f reais por dia.%n", lucro);
+    }
+}
