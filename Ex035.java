aa07b45e7bc7161a642681be8123d08ca38ec147
Isadora Brito <isadoradpbrito@gmail.com>
2021-01-29 22:01:48 -0300

    lista02

--- Ex035.java
+++ Ex035.java
@@ -4,6 +4,22 @@ public class Ex035 {
 
         Scanner in = new Scanner(System.in);
 
+        int estoqueatual,estoquemin,estoquemax,qmedia;
 
+        System.out.print("Digite a quantidade atual do estoque: ");
+        estoqueatual = in.nextInt();
+        System.out.print("Digite a quantidade minima e maxima em estoque do produto: ");
+        estoquemin = in.nextInt();
+        estoquemax = in.nextInt();
+
+        qmedia = (estoquemax + estoquemin)/2;
+
+        System.out.println("A quantidade média de estoque é "+qmedia);
+
+        if (estoqueatual>=qmedia) {
+            System.out.println("Nao efetuar compra.");
+        } else {
+            System.out.println("Efetuar compra.");
+        }
     }
 }
