da4c00e0f4e04e236b6e37cd10813edd045de49f
Isadora Brito <isadoradpbrito@gmail.com>
2021-02-01 14:01:19 -0300

    lista04

---
+++ Ex057.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Ex057 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int valor;
+
+        System.out.print("Digite um valor entre 1 e 10: ");
+        valor = in.nextInt();
+
+        while (valor < 1 || valor > 10) {
+            System.out.print("Digite um valor entre 1 e 10: ");
+            valor = in.nextInt();
+        }
+        System.out.println(valor);
+    }
+}
