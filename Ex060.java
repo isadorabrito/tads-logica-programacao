60d734a57f9a22e64f9eb81712baee43785130b9
Isadora Brito <isadoradpbrito@gmail.com>
2021-02-01 15:34:52 -0300

    lista04

---
+++ Ex060.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Ex060 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int idade;
+
+        System.out.print("Digite uma idade válida: ");
+        idade = in.nextInt();
+
+        while (idade <= 0 || idade >= 150) {
+            System.out.println("Digite uma idade válida: ");
+            idade = in.nextInt();
+        }
+        System.out.println(idade+" anos.");
+    }
+}
