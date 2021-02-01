b08a3e31d1363b8f4f25b9dc7d88d29266c76ded
Isadora Brito <isadoradpbrito@gmail.com>
2021-02-01 16:07:32 -0300

    lista04

---
+++ Ex063.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Ex063 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int valor;
+
+        System.out.print("Digite um valor: ");
+        valor = in.nextInt();
+
+        while (valor % 5 != 0) {
+        System.out.print("Digite um valor:");
+        valor = in.nextInt();
+        }
+        System.out.println("O valor "+valor+" é multiplo de 5.");
+    }
+}
