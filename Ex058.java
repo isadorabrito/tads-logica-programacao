e19ee2c78fabab5b3a5bf571d9b724ceadd10978
Isadora Brito <isadoradpbrito@gmail.com>
2021-02-01 14:07:30 -0300

    lista04

---
+++ Ex058.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Ex058 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int cont, senha;
+        cont = 1;
+
+        System.out.print("Digite sua senha: ");
+        senha = in.nextInt();
+
+        while (senha!=12345) {
+            System.out.print("Digite sua senha: ");
+            senha = in.nextInt();
+            cont++;
+        }
+        System.out.printf("%d tentativas até a senha correta.%n", cont);
+    }
+}
