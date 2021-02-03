21315e2ad3886e37f7844840020eb2dfec220d9e
Isadora Brito <isadoradpbrito@gmail.com>
2021-02-03 16:43:49 -0300

    lista04

--- Ex077.java
+++ Ex077.java
@@ -5,12 +5,15 @@ public class Ex077 {
         Scanner in = new Scanner(System.in);
 
         int v, v1 , v2, v3, vb, vn, total;
+        String vencedor;
+
         v1 = 0; 
         v2 = 0; 
         v3 = 0;
         vb = 0; 
         vn = 0;
         v = 0;
+        
         while (v >= 0) {
         System.out.println("Digite seu voto: \n\t[1] para candidato 1 \n\t[2] para candidato 2 \n\t[3] para candidato 3 \n\t[4] para branco \n\t[5] para nulo. \n\t[-1] para sair.");
         v = in.nextInt();
@@ -25,17 +28,18 @@ public class Ex077 {
         } else if (v == 5) {
             vn++;
         } else {
-            System.out.println("opção incorreta.");
+            System.out.println("FIM");
         }
         }
-        if (v1>v2 && v1 > v3) {
-            System.out.println("o vencedor é o candidato 1 com "+v1+" votos.");
-        } else if (v2>v3 && v2>v1) {
-            System.out.println("o vencedor é o candidato 2 com "+v2+" votos.");
+        if (v1 > v2 && v1 > v3) {
+            vencedor = "1";
+        } else if (v2 > v3 && v2 > v1) {
+            vencedor = "2";
         } else {
-            System.out.println("o vencedor é o candidato 3 com "+v3+" votos.");
+            vencedor = "3";
         }
         total = v1+v2+v3+vb+vn;
+        System.out.println("O vencedor é o candidato "+vencedor);
         System.out.println("Numero de votos em branco: "+vb);
         System.out.println("Numero de votos nulos: "+vn);
         System.out.println("Numero de eleitores que compareceram as urnas: "+total);       
---
+++ Ex079.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Ex079 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int valor;
+        int nota100=0,nota50=0,nota10=0,nota5=0,nota1=0;
+
+        System.out.print("Digite o valor do produto: R$ ");
+        valor = in.nextInt();
+
+        while (valor != 0) {
+            nota100 = valor/100;
+            valor %=100;
+            nota50 = valor/50;
+            valor %=50;
+            nota10 = valor/10;
+            valor %=10;
+            nota5 = valor/5;
+            valor %=5;
+            nota1 = valor/1;
+
+        System.out.println(nota100+ " notas de 100.");
+        System.out.println(nota50+ " notas de 50.");
+        System.out.println(nota10+ " notas de 10.");
+        System.out.println(nota5+ " notas de 5.");
+        System.out.println(nota1+ " notas de 1.");
+        
+        System.out.print("Digite o valor do produto ou 0 para sair: R$ ");
+        valor = in.nextInt();
+        }
+    }
+}
