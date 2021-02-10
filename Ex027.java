c5838b56f929813817a6a07fa0e754c1f020c880
Isadora Brito <isadoradpbrito@gmail.com>
2021-02-09 21:20:16 -0300

    lista02

--- Ex027.java
+++ Ex027.java
@@ -1,21 +1,23 @@
 import java.util.Scanner;
+
 public class Ex027 {
     public static void main(String[] args) {
-        
+
         Scanner in = new Scanner(System.in);
 
-        int horaini,horafim,tempojogo;
+        int horaini, horafim, tempojogo;
 
         System.out.print("Digite a hora de inicio da partida: ");
         horaini = in.nextInt();
         System.out.print("Digite a hora do fim da partida: ");
         horafim = in.nextInt();
 
-        tempojogo = horafim - horaini;
-
-        if (horafim < horaini){
-        tempojogo +=12;
-        }     
-        System.out.println("O tempo de jogo foi "+tempojogo+ " horas.");
+        if (horafim > horaini) {
+            tempojogo = horafim - horaini;
+            System.out.println("O tempo de jogo foi " + tempojogo + " horas.");
+        } else {
+            tempojogo = (24 - horaini + horafim);
+            System.out.println("O tempo de jogo foi " + tempojogo + " horas.");
+        }
     }
 }
