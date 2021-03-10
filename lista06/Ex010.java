1650273060ff00b6c23799a5a1260bde7b32c36b
Isadora Brito <isadoradpbrito@gmail.com>
2021-03-10 16:36:06 -0300

    lista06

---
+++ lista06/Ex010.java
@@ -0,0 +1,23 @@
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+import java.util.function.DoubleBinaryOperator;
+public class Ex010 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int nlinhas, dobro; 
+
+        System.out.print("Digite o numero de linhas: ");
+        nlinhas = in.nextInt();
+        dobro = (nlinhas * 2);
+
+        for (int l = 1; l <= nlinhas; l++) {
+            for (int a = 1; a < dobro ; a++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+            dobro -= 2;
+        }
+    }
+}
