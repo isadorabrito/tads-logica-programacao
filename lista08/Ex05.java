0c6caa1143e617e06ffbc6742dc72bb3bb7c1e28
Isadora Brito <isadoradpbrito@gmail.com>
2021-03-21 05:57:05 -0300

    lista08

--- lista08/Ex05.java
+++ lista08/Ex05.java
@@ -6,22 +6,25 @@ public class Ex05 {
         Scanner in = new Scanner(System.in);
 
         int[] vetor = new int[20];
-        int aux;
+        int aux, j=10;
 
         for (int i = 0; i < vetor.length; i++) {
             System.out.print("Digite um numero: ");
             vetor[i] = in.nextInt();
         }
+
         System.out.print("Vetor: ");
         for (int i = 0; i < vetor.length; i++) {
             System.out.print(vetor[i] + " ");
         }
+
         System.out.println();
 
         for (int i = 0; i < vetor.length / 2; i++) {
-            aux = vetor[i];
-            vetor[i] = vetor[19 - i];
-            vetor[19 - i] = aux;
+            aux = vetor[j];
+            vetor[j] = vetor[i];
+            vetor[i] = aux;
+            j++;
         }
         System.out.print("Vetor modificado: ");
         for (int i = 0; i < vetor.length; i++) {
