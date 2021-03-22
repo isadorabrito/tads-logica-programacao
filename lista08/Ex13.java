8b96724ab9eba6552a5f85dc6635c2cf5e8fd579
Isadora Brito <isadoradpbrito@gmail.com>
2021-03-22 06:05:35 -0300

    lista08

---
+++ lista08/Ex13.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Ex13 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int[] vetor = new int[12];
+        int x = 0, y = 0, soma = 0;
+
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print("Digite o valor do vetor na posição " + i + ": ");
+            vetor[i] = in.nextInt();
+        }
+
+        while (x >= 0 || y >= 0) {
+            System.out.print("Digite o valor de X e Y respectivamente: ");
+            x = in.nextInt();
+            y = in.nextInt();
+
+            if (x == -1 || y == -1) {
+                break;
+            }
+
+            System.out.print("Vetor: [");
+            for (int i = 0; i < vetor.length; i++) {
+                if (i == vetor.length - 1) {
+                    System.out.print(vetor[i] + "]");
+                } else {
+                    System.out.print(vetor[i] + " ");
+                }
+            }
+            System.out.println();
+
+            for (int i = 0; i < vetor.length; i++) {
+                soma = vetor[x] + vetor[y];
+            }
+
+            System.out.println("A soma da posição x e y é " + soma);
+        }
+    }
+}
