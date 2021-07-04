b02038e47bdba6e705fcdeb0d25acf8d9742e652
Isadora Brito <isadoradpbrito@gmail.com>
2021-07-04 01:05:55 -0300

    lista08

--- lista08/Ex08.java
+++ lista08/Ex08.java
@@ -6,36 +6,27 @@ public class Ex08 {
         Scanner in = new Scanner(System.in);
 
         int[] vetor = new int[20];
-        int elemento, maiores = 0, menores = 0, iguais = 0;
+        int posicao, maiores = 0, menores = 0, iguais = 0;
 
         for (int i = 0; i < vetor.length; i++) {
             System.out.println("Digite o elemento da posição " + (i) + ": ");
             vetor[i] = in.nextInt();
         }
 
-        System.out.print("Vetor: [");
-        for (int i = 0; i < vetor.length; i++) {
-            if (i == vetor.length - 1) {
-                System.out.println(vetor[i] + "]");
-            } else {
-                System.out.print(vetor[i] + " ");
-            }
-        }
-
-        System.out.println("Escolha um elemento do vetor: ");
-        elemento = in.nextInt();
+        System.out.println("Escolha uma posicao do vetor: ");
+        posicao = in.nextInt();
 
         for (int i = 0; i < vetor.length; i++) {
-            if (vetor[elemento] < vetor[i]) {
+            if (vetor[posicao] < vetor[i]) {
                 maiores++;
             }
-            if (vetor[elemento] > vetor[i]) {
+            if (vetor[posicao] > vetor[i]) {
                 menores++;
             }
-            if (vetor[elemento] == vetor[i]) {
+            if (vetor[posicao] == vetor[i]) {
                 iguais++;
-            }      
+            }
         }
-        System.out.println("O elemento [" + elemento + "] possui: \n" + maiores + " elemento(s) maior(es) que ele; \n" + menores + " elemento(s) menor(es) que ele; \n" + iguais + " elemento(s) igual(is) a ele.");
+        System.out.println(maiores + " elemento(s) maior(es) que ele; \n" + menores + " elemento(s) menor(es) que ele; \n" + (iguais - 1) + " elemento(s) igual(is) a ele.");
     }
 }
